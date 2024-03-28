0068ebb9e4fa908ab63de6048cfd25c0d629c2f3
panasche <shivamkumar40914@gmail.com>
2024-03-28 12:08:41 +0530

    edit college details api

--- server/routes/editInfoUserProfile.js
+++ server/routes/editInfoUserProfile.js
@@ -47,4 +47,51 @@ router.post('/editPersonalDetail', (req, res) =>{
     }
 })
 
+//edit college detail
+router.post('/collegeDetail', (req,res) => {
+    const {username, _id, collegeName, collegeLocation, branch, year, stream} = req.body;
+    if(username && _id && (collegeName || collegeLocation || branch || year || stream)){
+        userModel.findOne({ 'personalDetail.username' : username})
+        .then((userFound) => {
+            if(!userFound){
+                return res.status(400).json({error : "User not found!"});
+            }
+            const collegeIndex = userFound.collegeDetail.findIndex((college) => college._id.toString() === _id);
+            if (collegeIndex === -1) {
+                return res.status(400).json({ error: 'College does not exist!' });
+            }
+            const collegeToUpdate = userFound.collegeDetail[collegeIndex];
+            if (!collegeToUpdate.isValid) {
+                return res.status(404).json({ error: 'College is invalid!' });
+            }
+            userModel.findOneAndUpdate(
+                { 'personalDetail.username': username, 'collegeDetail._id': _id },
+                {
+                $set: {
+                    'collegeDetail.$.collegeName': collegeName || collegeToUpdate.collegeName,
+                    'collegeDetail.$.collegeLocation': collegeLocation || collegeToUpdate.collegeLocation,
+                    'collegeDetail.$.branch': branch || collegeToUpdate.branch,
+                    'collegeDetail.$.year': year || collegeToUpdate.year,
+                    'collegeDetail.$.stream': stream || collegeToUpdate.stream,
+                },
+                },
+                { new: true })
+                .then((infoUpdated) => {
+                    if (!infoUpdated) {
+                    return res.status(400).json({ error: 'User not found!' });
+                    }
+                    res.json({ message: 'College details updated successfully!', detail: infoUpdated.collegeDetail[collegeIndex] });
+                })
+                .catch((err) => {
+                    console.error(err);
+                    res.status(500).json({ error: 'Internal server error' });
+                }
+            );
+        })
+    }
+    else{
+        res.json({error : "Fields empty!"}); //better error message (issue)
+    }
+})
+
 module.exports = router
