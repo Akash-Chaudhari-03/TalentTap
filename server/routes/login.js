71d9dc11a04dde4d2dfb60942049a6182830dbd8
Khushi <92222438+kanscerr@users.noreply.github.com>
2024-06-29 14:05:19 +0530

    Update login.js

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const userModel = require('../schema/users');
-const bcrypt = require('bcrypt');  // Assuming bcrypt is used for hashing passwords
+const bcrypt = require('bcrypt'); 
 
 // Login existing user
 router.post('/user', (req, res) => {
