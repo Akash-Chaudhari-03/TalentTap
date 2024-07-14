143f7c35234914b32c1e3be1d521d3e931a4d7d9
kanscerr <92222438+kanscerr@users.noreply.github.com>
2024-07-14 13:14:34 +0530

    changes made

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const loginUser = require('./server/routes/login');
 // const updateUserProfile = require('./server/routes/addInfoUserProfile');
 const deleteInfoUserProfile = require('./server/routes/deleteInfoUserProfile');
 const getUserDetails = require('./server/routes/getUserDetails');
-const editUserDetails = require('./server/routes/editInfoUserProfile');
+// const editUserDetails = require('./server/routes/editInfoUserProfile');
 
 //add details api import
 const addPersonalDetail = require('./server/routes/addUserInfo/personalInfo');
@@ -16,7 +16,12 @@ const addSkills = require('./server/routes/addUserInfo/skills');
 const addCertificate = require('./server/routes/addUserInfo/certificate');
 const addProjects = require('./server/routes/addUserInfo/projects');
 
-
+//edit details api import
+const editPersonalDetail = require('./server/routes/editUserInfo/personalInfo');
+const editCollegeDetail = require('./server/routes/editUserInfo/collegeDetail');
+// const editSkills = require('./server/routes/editUserInfo/skills');
+const editCertificate = require('./server/routes/editUserInfo/certificate');
+// const editProjects = require('./server/routes/editUserInfo/projects');
 
 const app = express();
 const PORT = 3000;
@@ -45,8 +50,16 @@ app.use('/userInfo/addDetails/certificate', addCertificate);
 app.use('/userInfo/addDetails/project', addProjects);
 
 
-app.use('/userInfo/deleteDetails', deleteInfoUserProfile);
-app.use('/userInfo/editDetails', editUserDetails);
+//route calls for editing details
+app.use('/userInfo/editDetails/personalInfo', editPersonalDetail);
+app.use('/userInfo/editDetails/collegeDetail', editCollegeDetail);
+// app.use('/userInfo/editDetails/skills', editSkills);
+app.use('/userInfo/editDetails/certificate', editCertificate);
+// app.use('/userInfo/editDetails/project', editProjects);
+
+
+// app.use('/userInfo/deleteDetails', deleteInfoUserProfile);
+// app.use('/userInfo/editDetails', editUserDetails);
 
 //listening to server at port
 app.listen(PORT, () => console.log(`server running on http://localhost:${PORT}`));
